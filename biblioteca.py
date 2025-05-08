34f06af3c2bfb0cb946e965d8beba150700bdc50
Vitor Oliveira <vitorloliveira96@gmail.com>
2025-05-08 11:40:29 -0300

    atualização, atividade de conta de banco

--- biblioteca.py
+++ biblioteca.py
@@ -18,6 +18,12 @@ class Pessoa():
         else:
             self.falando = True
             print("Começou a falar")
+    def parar_falar(self):
+        if self.falando:
+            self.falando = False
+            print("Parou de falar")
+        else:
+            print("Já parou de falar")
 
     def comer(self):
         if self.comendo:
@@ -29,6 +35,12 @@ class Pessoa():
         else:
             self.comendo = True
             print("Começou  a comer")
+    def parar_comer(self):
+        if self.comendo:
+            self.comendo = False
+            print("Parou de comer")
+        else:
+            print("Já parou de comer")
 
     def dormir(self):
         if self.comendo:
@@ -40,3 +52,55 @@ class Pessoa():
         else:
             self.dormindo = True
             print(f"{self.nome} começou a dormir")
+    def acordar(self):
+        if self.dormindo:
+            self.dormindo = False
+            print("Acordou")
+        else:
+            print("Já está acordaro")
+
+
+class ContaBanco():
+    def __init__(self, num, nome, tipo):
+        self.num = num
+        self.nome = nome
+        self.tipo = tipo
+
+        self.status = False
+        self.saldo = 0
+
+    def ativarConta(self):
+        if self.status == False:
+            self.status = True
+        else:
+            print("A conta já está ativa")
+
+    def depositar(self, deposito):
+        if self.status == True:
+            self.saldo = deposito
+
+    def verificarSaldo(self):
+        print(self.saldo)
+
+    def sacar (self):
+
+        #Aqui precisa reformular para saque negativado.
+
+        if self.status == True:
+            if self.saldo == 0:
+                print("Você não tem saldo na conta")
+            elif self.saldo < 0:
+                print("Você está negtivado")
+            else:
+                print("Quanto deseja sacar")
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from biblioteca import Pessoa
+from biblioteca import Pessoa, ContaBanco
 
-aluno01 = Pessoa("Vitor", 29, 57 )
+"""aluno01 = Pessoa("Vitor", 29, 57 )
 print(f"{aluno01.nome} tem {aluno01.idade} anos e pesa {aluno01.peso}kg")
 
-
-aluno01.comer()
-aluno01.dormir()
 aluno01.falar()
+aluno01.parar_falar()
+aluno01.comer()
+aluno01.parar_comer()
+aluno01.dormir()
+aluno01.acordar()
+aluno01.acordar()"""
 
+cliente = ContaBanco("12354","Vitor","Corrente")
+
+cliente.ativarConta()
+cliente.depositar()
+cliente.verificarSaldo()
+cliente.sacar()
